32142d2b9d1d810c0244151aa33eb0a1d7aea851
misbahdn <34958260+misbahdn@users.noreply.github.com>
2019-02-11 01:59:46 +0700

    new
    new

---
+++ GALERI/folio.js
@@ -0,0 +1,23 @@
+
+var target_gambar = document.getElementById('target_gambar');
+var array_gambar = document.getElementById('asset').children;
+var no_sekarang = 0;
+
+function ganti_gambar(gambar){
+  target_gambar.src = gambar.getAttribute('src');
+  no_sekarang = gambar.className;
+}
+
+function left1(){
+  no_sekarang = no_sekarang - 1;
+  if(no_sekarang < 0) no_sekarang = 6;
+
+  target_gambar.src = array_gambar[no_sekarang].getAttribute('src');
+}
+
+function right1(){
+  no_sekarang = no_sekarang + 1;
+  if(no_sekarang > 6) no_sekarang = 0;
+
+  target_gambar.src = array_gambar[no_sekarang].getAttribute('src');
+}
